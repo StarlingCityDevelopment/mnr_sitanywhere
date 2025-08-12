3bc358535fdbffe9972d6b1cffbcb17c3abd1be6
IlMelons <melonss2002@gmail.com>
2025-08-12 10:26:52 +0200

    Cleaned Bridge

--- bridge/client/target/ox_target.lua
+++ bridge/client/target/ox_target.lua
@@ -1,21 +1,23 @@
 ---@diagnostic disable: duplicate-set-field, lowercase-global
 
-if GetResourceState("ox_target") ~= "started" then return end
+if GetResourceState('ox_target') ~= 'started' then return end
 
-local state = require "client.state"
+local state = require 'client.state'
 
 target = {}
 
 function target.AddModels(models)
     exports.ox_target:addModel(models, {
         {
-            label = locale("target.sit"),
-            name = "mnr_sitanywhere:sit",
-            icon = "fa-solid fa-chair",
+            label = locale('target.sit'),
+            name = 'mnr_sitanywhere:sit',
+            icon = 'fa-solid fa-chair',
             canInteract = function(entity)
                 return DoesEntityExist(entity) and not state.sitting
             end,
-            event = "mnr_sitanywhere:client:Sit",
+            onSelect = function(data)
+                TriggerEvent('mnr_sitanywhere:client:Sit', data)
+            end,
         }
     })
 end
--- bridge/client/target/qb-target.lua
+++ bridge/client/target/qb-target.lua
@@ -1,24 +1,25 @@
 ---@diagnostic disable: duplicate-set-field, lowercase-global
 
-if GetResourceState("qb-target") ~= "started" then return end
+if GetResourceState('qb-target') ~= 'started' then return end
 
-local state = require "client.state"
+local state = require 'client.state'
 
 target = {}
 
 function target.AddModels(models)
-    exports["qb-target"]:AddTargetModel(models, {
+    exports['qb-target']:AddTargetModel(models, {
         options = {
             {
-                type = "client",
-                event = "mnr_sitanywhere:client:Sit",
-                icon = "fas fa-chair",
-                label = locale("target.sit"),
+                label = locale('target.sit'),
+                icon = 'fa-solid fa-chair',
                 canInteract = function(entity)
                     return DoesEntityExist(entity) and not state.sitting
-                end
+                end,
+                action = function(entity)
+                    TriggerEvent('mnr_sitanywhere:client:Sit', { entity = entity })
+                end,
             }
         },
-        distance = 3.0,
+        distance = 2.0,
     })
 end
