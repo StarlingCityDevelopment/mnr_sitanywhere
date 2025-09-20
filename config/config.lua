ef0ddf7bff4d9c1aa5c30509461230e9c2c25def
Codexis <contact@codexis.dev>
2025-09-20 12:32:27 +0200

    chore: setup starling

--- config/config.lua
+++ config/config.lua
@@ -219,7 +219,4 @@ return {
 			[2] = vec4(0.3, -0.05, 0.0, 180.0),
 		},
 	},
-	---@description Paste the presets here 🔽 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/sitanywhere]
-
-	---@description Paste the presets here 🔼 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/sitanywhere]
 }
